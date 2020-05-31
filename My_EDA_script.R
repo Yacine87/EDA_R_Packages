0140fa16348480751b2f9c64c38e2d15007e7c0c
Yacine Hafiane <yacine.hafiane@abc-dz.net>
2020-05-31 22:33:11 +0200

    My_EDA_script

---
+++ My_EDA_script.R
@@ -0,0 +1,55 @@
+### The tidyverse way ************************************************************************************
+
+# Install packages 
+# install.packages("tidyverse")
+
+# Load packages 
+library(tidyverse)
+library(readr) # optional cause its per default loaded 
+
+# Import data 
+
+# Discoer data 
+
+# EDA - With tabular tables 
+
+# EDA - Testing relationships among variables 
+
+# EDA - data vizualisation 
+
+
+### SmartEDA way ************************************************************************************
+# install.packages("devtools") 
+library(devtools)
+# Install from the github repo 
+devtools::install_github("daya6489/SmartEDA",ref = "develop")
+# Or
+install.packages("SmartEDA")
+# Load the package 
+library(SmartEDA)
+
+### dlookr way ************************************************************************************
+
+### DataExplorer way ************************************************************************************
+
+### Hmisc way ************************************************************************************
+
+### exploreR way ************************************************************************************
+
+### RtutoR way ************************************************************************************
+
+### summarytools way ************************************************************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
